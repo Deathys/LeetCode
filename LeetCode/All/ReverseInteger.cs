fee6e24fba312128f758c76fce4e9d0308024f16
SlavaAs <slava2221@gmail.com>
2025-12-09 21:47:42 +0100

    added additinal solution for ReverseInteger

--- LeetCode/All/ReverseInteger.cs
+++ LeetCode/All/ReverseInteger.cs
@@ -4,7 +4,7 @@ namespace LeetCode.All;
 
 public class ReverseInteger
 {
-    public int Reverse(int x) 
+    public int Reverse2(int x) 
     {
         string xStr = x.ToString();
         string sign = string.Empty;
@@ -22,5 +22,26 @@ public class ReverseInteger
         }
 
         return 0;
+    }
+
+    public int Reverse(int x) 
+    {
+        int result = 0;
+        
+        while (x != 0) 
+        {
+            int digit = x % 10;
+            x /= 10;
+            
+            // Check for overflow before multiplying
+            if (result > int.MaxValue / 10 || result < int.MinValue / 10) 
+            {
+                return 0;
+            }
+            
+            result = result * 10 + digit;
+        }
+        
+        return result;
     }
 }
